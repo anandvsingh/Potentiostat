883e3b22471befb7d54719a861338c6451cd59d0
anandvsingh <anand_vardhan_singh@outlook.com>
2018-06-21 17:39:57 +0530

    Fixed errors caused by the use of Sprintf by making a user defined function itostring

--- cheapstat.c
+++ cheapstat.c
@@ -85,7 +85,31 @@ int buttonHandler(profile profiles[PROFILES_LENGTH], uint8_t* status, uint8_t* p
 
 int16_t CV_test (char* name, int16_t slope, int16_t start, int16_t stop, int16_t scans, int16_t sample_rate, uint8_t curr_range);
 
-
+int itostring(int num, char* Resultstr)
+{
+  int t1,len=0,temp = num,Strl;
+  if (num<0)
+  {
+	  len++;
+	  *Resultstr='-';
+	  num*=-1;
+  }
+  while (temp)
+  {
+    len++;
+    temp/=10;
+  }
+  Strl = len;
+  temp = num;
+  *(Resultstr+len--)='\0';
+  while (temp)
+  {
+    t1=temp%10;
+	temp/=10;
+    *(Resultstr+len--) = t1+48;
+  }
+  return Strl;
+}
 
 
 USART_data_t USART_data;
@@ -93,7 +117,8 @@ USART_data_t USART_data;
 void send_string(char* string);
 int main()
 {
- char Resultstr[12];
+ char Resultstr[18];
+ uint8_t lengthOfString;
  uint8_t status;
  uint8_t profile_index;
  uint8_t profile_opt_index;
@@ -258,18 +283,23 @@ int main()
    lcdHome(); //Equivalent to lcdGotoXY(0,0)
    lcdPrintData("StDev = ",8);
    StDev = (int) StDev; //Because Sprintf can only handle integers so typecasting is done
-   sprintf(Resultstr,"%8d",StDev); //Because lcdPrintData can only handle strings, value is stored in string
-   lcdPrintData(Resultstr,8);
+   //sprintf(Resultstr,"%4d",StDev); //Because lcdPrintData can only handle strings, value is stored in string
+   lengthOfString = itostring (StDev,Resultstr); 
+   lcdPrintData(Resultstr,lengthOfString);
    lcdGotoXY(0,1);
    mean = (int) mean; //Because Sprintf can only handle integers so typecasting is done
    lcdPrintData("Mean = ",7);
-   sprintf(Resultstr,"%8d",mean); //Because lcdPrintData can only handle strings, value is stored in string
-   lcdPrintData(Resultstr,8);
+   //sprintf(Resultstr,"%4d",mean); //Because lcdPrintData can only handle strings, value is stored in string
+   lengthOfString = itostring (mean,Resultstr); 
+   lcdPrintData(Resultstr,lengthOfString);
    lcdGotoXY(0,2);
    HiVal = (int) HiVal; //Because Sprintf can only handle integers so typecasting is done
    lcdPrintData("Max = ",6);
-   sprintf(Resultstr,"%8d",HiVal); //Because lcdPrintData can only handle strings, value is stored in string
-   lcdPrintData(Resultstr,8);
+   //sprintf(Resultstr,"%7d",1010101); //Because lcdPrintData can only handle strings, value is stored in string
+   //sprintf(Resultstr,"%5d",HiVal);
+   lengthOfString = itostring (HiVal,Resultstr); 
+   lcdPrintData(Resultstr,lengthOfString);
+   //lcdPrintData(Resultstr,5);
    while(buttonHandler(profiles,&status,&profile_index,&profile_opt_index,&profile_edit_index,&profile_edit_sel,&length)!=1) {}
 
   }
@@ -427,7 +457,7 @@ int16_t CV_test (char* name, int16_t slope, int16_t start, int16_t stop, int16_t
  float st,st2,st3;
  unsigned long variance;
  bool up;
-
+ long num, decimal_num, remainder, base = 1, binary = 0, no_of_1s = 0;
  int16_t current_DAC, min_DAC, max_DAC;
 
 //storing ADC results
@@ -630,9 +660,9 @@ StDev = sqrt(variance); //Calculated standard deviation of the response of the s
 HiVal = current[i/2];
  for(j = 0; j < i; j++)
  {
-  do{} while(!USART_IsTXDataRegisterEmpty(&USARTC0));
+  do{} while(!USART_IsTXDataRegisterEmpty(&USARTC0)); //Data is sent as high bit and low bit
   USART_PutChar(&USARTC0, current[j]>>8);
-  do{} while(!USART_IsTXDataRegisterEmpty(&USARTC0));
+  do{} while(!USART_IsTXDataRegisterEmpty(&USARTC0)); //Low bit is sent
   USART_PutChar(&USARTC0, current[j]);
   if (j>=i/2)
   {
@@ -641,6 +671,8 @@ HiVal = current[i/2];
     sum += current[j];
   }
  }
+
+ 
  mean = sum/(i/2); //To calculate mean of the datapoints obtained in the second cycle
 
 
